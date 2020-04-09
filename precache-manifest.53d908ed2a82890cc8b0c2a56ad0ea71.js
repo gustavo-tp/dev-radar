137392343c1cacbf6abbc4f03c179646ae64f96d
gustavo-tp <gustavo1190.gt@gmail.com>
2020-04-08 23:49:27 -0300

    Updates

---
+++ precache-manifest.53d908ed2a82890cc8b0c2a56ad0ea71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aecf14392afa085c4e43d4bc2198a4f",
+    "url": "/dev-radar/index.html"
+  },
+  {
+    "revision": "6abc7819879cf20d2cdc",
+    "url": "/dev-radar/static/css/main.135adfce.chunk.css"
+  },
+  {
+    "revision": "e19ded47508c791c279d",
+    "url": "/dev-radar/static/js/2.733b4ed7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dev-radar/static/js/2.733b4ed7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6abc7819879cf20d2cdc",
+    "url": "/dev-radar/static/js/main.e6780b51.chunk.js"
+  },
+  {
+    "revision": "92dd18219e7c3d9d17d0",
+    "url": "/dev-radar/static/js/runtime-main.4ea54224.js"
+  }
+]);
